afc24db48444b4b117f215a205510b21083d9921
Dustin Murdock <dmurdock2@gmail.com>
2020-07-11 00:22:56 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace random_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            List<int> numbers = new List<int> {
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+                };
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers.Contains(i)) {
+                    Console.WriteLine($"numbers list contains {i}");
+                } else {
+                    Console.WriteLine($"numbers list does not contain {i}");
+                }
+
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+# Practice: Random Numbers
+
+
+## Instructions
+1. Create a list of random numbers. Each number will be between 0 and 5.
+1. Determine if the current loop index is contained inside of the `numbers` list. Print a message to the console indicating whether the index is in the list.
+
+
+#### Example Output in the Terminal
+```sh
+numbers list contains 0
+numbers list does not contain 1
+numbers list does not contain 2
+numbers list contains 3
+numbers list contains 4
+numbers list does not contains 5
+```
+**NOTE:** Each run will produce different output.
